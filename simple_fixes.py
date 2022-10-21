6e3ba3589310b911a9898c2240d7e4935aa4567f
Lukas Winkler <git@lw1.at>
2022-10-21 15:36:19 +0200

    apply some simple replacements

---
+++ simple_fixes.py
@@ -0,0 +1,25 @@
+"""
+fix a few common mistakes
+
+based on
+https://stackoverflow.com/a/15448887
+"""
+import re
+from pathlib import Path
+
+current_dir = Path(".")
+
+replacements = {
+    "Mitoma": "Matomo",
+    "Matoma": "Matomo",
+    "matoma": "matomo",
+    "Matomo Camp":"MatomoCamp"
+}
+search_strings = [re.escape(k) for k in sorted(replacements, key=len, reverse=True)]
+pattern = re.compile("|".join(search_strings), flags=re.DOTALL)
+
+for srt_file in current_dir.glob("**/*.srt"):
+    text = srt_file.read_text()
+    fixed_text=pattern.sub(lambda x: replacements[x.group(0)], text)
+    srt_file.write_text(fixed_text)
+
