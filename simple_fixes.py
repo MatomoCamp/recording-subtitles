4ad0ac524316de9e96c668c6dca1a72e91969ce4
Lukas Winkler <git@lw1.at>
2022-10-25 14:29:24 +0200

    more replacements

--- simple_fixes.py
+++ simple_fixes.py
@@ -13,13 +13,13 @@ replacements = {
     "Mitoma": "Matomo",
     "Matoma": "Matomo",
     "matoma": "matomo",
-    "Matomo Camp":"MatomoCamp"
+    "Matomo Camp": "MatomoCamp",
+    "Big Blue Button": "BigBlueButton"
 }
 search_strings = [re.escape(k) for k in sorted(replacements, key=len, reverse=True)]
 pattern = re.compile("|".join(search_strings), flags=re.DOTALL)
 
 for srt_file in current_dir.glob("**/*.srt"):
     text = srt_file.read_text()
-    fixed_text=pattern.sub(lambda x: replacements[x.group(0)], text)
+    fixed_text = pattern.sub(lambda x: replacements[x.group(0)], text)
     srt_file.write_text(fixed_text)
-
