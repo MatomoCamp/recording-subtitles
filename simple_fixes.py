054369a78e50bfee570231e49408fa66f0600cc5
Lukas Winkler <git@lw1.at>
2022-12-03 22:45:44 +0100

    quick fixes

--- simple_fixes.py
+++ simple_fixes.py
@@ -16,10 +16,16 @@ replacements = {
     "matoma": "matomo",
     "Matamow": "Matomo",
     "Matahomo": "Matomo",
+    "Matamua": "Matomo",
+    "Mitsuama": "Matomo",
+    "Biwik": "Piwik",
     "Matomo Camp": "MatomoCamp",
+    "Matomo camp": "MatomoCamp",
     "matamocamp": "matomocamp",
     "matumocamp": "matomocamp",
     "motomocamp": "matomocamp",
+    "MatomoCam ": "MatomoCamp ",
+    "MatomoCampp": "MatomoCamp",
     "Big Blue Button": "BigBlueButton",
     "Lucas": "Lukas"
 }
@@ -28,5 +34,7 @@ pattern = re.compile("|".join(search_strings), flags=re.DOTALL)
 
 for srt_file in current_dir.glob("**/*.srt"):
     text = srt_file.read_text()
-    fixed_text = pattern.sub(lambda x: replacements[x.group(0)], text)
+    fixed_text, n = pattern.subn(lambda x: replacements[x.group(0)], text)
+    if n:
+        print(f"replaced {n} words in {srt_file}")
     srt_file.write_text(fixed_text)
